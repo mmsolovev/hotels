67ca81bc13da4020ba48e0249a86bd2d41ac434f
Mikhail Solovev <mmsolovevmail@gmail.com>
2025-09-04 21:51:29 +0300

    add: Exceptions

--- users/dependencies.py
+++ users/dependencies.py
@@ -1,16 +1,18 @@
 from datetime import datetime
 
-from fastapi import Request, HTTPException, Depends, status
+from fastapi import Request, Depends
 from jose import jwt, JWTError
 
 from config import settings
+from exceptions import TokenExpiredException, TokenAbsentException, IncorrectTokenFormatException, \
+    UserIsNotExistsException
 from users.services import UsersService
 
 
 def get_token(request: Request):
     token = request.cookies.get('booking_access_token')
     if not token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenAbsentException
     return token
 
 
@@ -20,18 +22,18 @@ async def get_current_user(token: str = Depends(get_token)):
             token, settings.AUTH_KEY, settings.ALGORITHM
         )
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='jwt')
+        raise IncorrectTokenFormatException
 
     expire: str = payload.get('exp')
     if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenExpiredException
 
     user_id: str = payload.get('sub')
     if not user_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserIsNotExistsException
 
     user = await UsersService.find_by_id(int(user_id))
     if not user_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserIsNotExistsException
 
     return user
