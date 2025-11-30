98b681fc358506ec38f05d075ffb2ab2a4bf1b08
Mikhail Solovev <81823776+mmsolovev@users.noreply.github.com>
2025-11-30 15:42:58 +0300

    add: unittests (#10)
    Co-authored-by: Mikhail Solovev <mmsolovevmail@gmail.com>

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", 'TEST', 'PROD']
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
@@ -11,8 +15,28 @@ class Settings(BaseSettings):
     ALGORITHM: str
 
     @property
-    def database_url(self):
-        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+    def database_url(cls, v):
+        v['DATABASE_URL'] = f"postgresql+asyncpg://{v['DB_USER']}:{v['DB_PASS']}@{v['DB_HOST']}:{v['DB_PORT']}/{v['DB_NAME']}"
+        return v
+
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+
+    @property
+    def test_database_url(cls, v):
+        v['TEST_DATABASE_URL'] = f"postgresql+asyncpg://{v['TEST_DB_USER']}:{v['TEST_DB_PASS']}@{v['TEST_DB_HOST']}:{v['TEST_DB_PORT']}/{v['TEST_DB_NAME']}"
+        return
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    AUTH_KEY: str
+    ALGORITHM: str
+
 
     model_config = SettingsConfigDict(env_file='.env')
 
--- database.py
+++ database.py
@@ -1,10 +1,21 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from config import settings
 
 
-engine = create_async_engine(settings.database_url)
+if settings.MODE == "TEST":
+    DATABASE_URL = settings.TEST_DATABASE_URL
+    DATABASE_PARAMS = {
+        "pullclass": NullPool,
+    }
+else:
+    DATABASE_URL = settings.DATABASE_URL
+    DATABASE_PARAMS = {}
+
+
+engine = create_async_engine(settings.DATABASE_URL, **DATABASE_PARAMS)
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
